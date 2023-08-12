039b4892e173000f364e4393c6fb7499625c6c8b
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2023-08-12 12:27:20 -0600

    📦 install dependncies to run tests
    - dependencies necesaries for tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -79,6 +79,7 @@ const config = {
   // An array of file extensions your modules use
   moduleFileExtensions: [
     "js",
+    "jsx",
   ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
@@ -167,19 +168,8 @@ const config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  transform:   {
-    '.*\\.(js?)$': [
-      '@swc/jest',
-      {
-        jsc: {
-          transform: {
-            react: {
-              runtime: 'automatic',
-            },
-          },
-        },
-      },
-    ],
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest'
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
