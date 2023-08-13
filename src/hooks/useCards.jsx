0922337c136dac77fceb397b080a323ff58f3d2e
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2023-08-13 16:59:48 -0600

    📝 update jsdoc
    - update jsdoc useCard.
    - update jsdoc useGame.

--- src/hooks/useCards.jsx
+++ src/hooks/useCards.jsx
@@ -10,6 +10,10 @@ import { CardsContext } from '../Provider/CardsProvider';
  * @returns {Array} context.cards - Array of card data.
  * @returns {boolean} context.loading - Loading state of the card data fetch.
  * @returns {Error|null} context.error - Any error that occurred during fetch.
+ * @returns {function} context.toggleSelectedCard - Function to toggle the selected state of a card.
+ * @returns {function} context.toggleDisabledCard - Function to toggle the disabled state of a card.
+ * @returns {function} context.handlePageChange - Function to handle the page change.
+ * @returns {function} context.setRestart - Function to restart the game.
  */
 export const useCards = () => {
   const context = React.useContext(CardsContext);
--- src/hooks/useGame.jsx
+++ src/hooks/useGame.jsx
@@ -15,7 +15,16 @@ import { GameContext } from '../Provider/GameProvider';
  * @returns {Array} Game context value.errors - List of errors occurred in the game.
  * @returns {Function} Game context value.setErrors - Function to set the list of errors.
  * @returns {boolean} Game context value.isWinner - Indicates if the player is a winner.
- * @returns {Function} Game context value.setIsWinner - Function to set the winner status.
+ * @returns {Function} Game context value.setIsNewGame - Function to set the new game state.
+ * @returns {boolean} Game context value.isNewGame - Indicates if the game is a new game.
+ * @returns {string} Game context value.difficulty - The difficulty of the game.
+ * @returns {Function} Game context value.setDifficulty - Function to set the difficulty of the game.
+ * @returns {boolean} Game context value.paused - Indicates if the game is paused.
+ * @returns {Function} Game context value.setPaused - Function to set the paused state of the game.
+ * @returns {Function} Game context value.handleNewGame - Function to handle the new game.
+ * @returns {Function} Game context value.handleStartGame - Function to handle the start of the game.
+ * @returns {Function} Game context value.handleSuccess - Function to handle the success of the game.
+ * @returns {Function} Game context value.handleError - Function to handle the error of the game.
  *
  * @throws Will throw an error if the hook is not used inside a Game Provider.
  */
