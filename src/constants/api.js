a177d3d41aacd6df2f3d2dca191d9bfcbf0db1ae
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2023-08-13 16:58:52 -0600

    🚧 api constants
    - create constants to handle difficulty.

---
+++ src/constants/api.js
@@ -0,0 +1,8 @@
+export const IMAGES_URL =
+  'https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries';
+
+export const DIFFICULTIES = {
+  easy: 10,
+  medium: 15,
+  hard: 20,
+};
