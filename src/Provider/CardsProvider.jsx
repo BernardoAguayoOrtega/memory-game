bfb5420bd0468732cf5388647241bdd3a6ab0594
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2023-08-13 18:32:29 -0600

    🚧 providers
    - card provider, add logic to toggle cards, refresh call.

--- src/Provider/CardsProvider.jsx
+++ src/Provider/CardsProvider.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
-import { IMAGES_URL } from '../constants/api';
+import { DIFFICULTIES, IMAGES_URL } from '../constants/api';
+import { transformCards } from '../utils/cards';
 
 /**
  * React context for cards data.
@@ -18,17 +19,64 @@ export const CardsContext = React.createContext();
  * @param {React.ReactNode} props.children - Child components.
  * @returns {React.Element} Provider component.
  */
-const CardsProvider = ({ children }) => {
+export const CardsProvider = ({ children }) => {
   const [cards, setCards] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
+  const [pageSize, setPageSize] = React.useState(DIFFICULTIES.easy);
+  const [restart, setRestart] = React.useState(false);
+
+  const toggleSelectedCard = React.useCallback(
+    id => {
+      const selectedCard = cards[id];
+      const newCard = { ...selectedCard, isToggle: !selectedCard.isToggle };
+      setCards(prevCards => {
+        const newCards = [...prevCards];
+        newCards[id] = newCard;
+        return newCards;
+      });
+    },
+    [cards]
+  );
+
+  const toggleDisabledCard = React.useCallback(
+    id => {
+      const selectedCard = cards[id];
+      const newCard = {
+        ...selectedCard,
+        isDisabled: !selectedCard.isDisabled,
+        isToggle: true,
+      };
+      setCards(prevCards => {
+        const newCards = [...prevCards];
+        newCards[id] = newCard;
+        return newCards;
+      });
+    },
+    [cards]
+  );
+
+  const handlePageChange = React.useCallback(
+    value => {
+      setLoading(true);
+      const pageSize = DIFFICULTIES[value] || DIFFICULTIES.easy;
+      setPageSize(pageSize);
+      setLoading(false);
+    },
+    [setPageSize]
+  );
 
   React.useEffect(() => {
     const fetchCards = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(IMAGES_URL);
-        setCards(response.data.entries);
+        const response = await axios.get(IMAGES_URL, {
+          params: {
+            per_page: pageSize,
+          },
+        });
+        const cards = transformCards(response?.data?.entries);
+        setCards(cards);
       } catch (error) {
         setError(error);
       } finally {
@@ -36,11 +84,26 @@ const CardsProvider = ({ children }) => {
       }
     };
     fetchCards();
-  }, []);
+  }, [pageSize, restart]);
 
   const value = React.useMemo(
-    () => ({ cards, loading, error }),
-    [cards, loading, error]
+    () => ({
+      cards,
+      loading,
+      error,
+      toggleSelectedCard,
+      toggleDisabledCard,
+      handlePageChange,
+      setRestart,
+    }),
+    [
+      cards,
+      loading,
+      error,
+      toggleSelectedCard,
+      toggleDisabledCard,
+      handlePageChange,
+    ]
   );
 
   return (
@@ -51,5 +114,3 @@ const CardsProvider = ({ children }) => {
 CardsProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
-
-export default CardsProvider;
--- src/Provider/GameProvider.jsx
+++ src/Provider/GameProvider.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useCards } from '../hooks/useCards';
+import useLocalStorage from '../hooks/useLocalStorage';
+import { DIFFICULTIES } from '../constants/api';
 
 /**
  * React context for game data.
@@ -16,23 +19,127 @@ export const GameContext = React.createContext();
  * @returns {React.Element} Provider component.
  */
 export function GameProvider({ children }) {
-  const [name, setName] = React.useState('');
+  const [name, setName] = useLocalStorage('name', '');
   const [asserts, setAsserts] = React.useState(0);
   const [errors, setErrors] = React.useState(0);
-  const [isWinner, setIsWinner] = React.useState(false);
+  const [isNewGame, setIsNewGame] = React.useState(true);
+  const [difficulty, setDifficulty] = useLocalStorage(
+    'difficulty',
+    DIFFICULTIES.easy
+  );
+  const [paused, setPaused] = React.useState(false);
+
+  const { current: successSound } = React.useRef(
+    new Audio('../../public/audio/success.mp3')
+  );
+  const { current: errorSound } = React.useRef(
+    new Audio('../../public/audio/wrong-answer.mp3')
+  );
+
+  const {
+    cards,
+    toggleSelectedCard,
+    toggleDisabledCard,
+    handlePageChange,
+    setRestart,
+  } = useCards();
+
+  const isWinner = React.useMemo(
+    () => asserts === cards.length / 2,
+    [asserts, cards.length]
+  );
+
+  const handleError = React.useCallback(
+    (firstCard, secondCard) => {
+      setPaused(true);
+      errorSound.play();
+      const timeout = setTimeout(() => {
+        toggleSelectedCard(firstCard.id);
+        toggleSelectedCard(secondCard.id);
+        setErrors(prevErrors => prevErrors + 1);
+        setPaused(false);
+      }, 500);
+
+      return () => clearTimeout(timeout);
+    },
+    [errorSound, toggleSelectedCard]
+  );
+
+  const handleSuccess = React.useCallback(
+    (firstCard, secondCard) => {
+      successSound.play();
+      toggleDisabledCard(firstCard.id);
+      toggleDisabledCard(secondCard.id);
+      setAsserts(prevAsserts => prevAsserts + 1);
+    },
+    [successSound, toggleDisabledCard]
+  );
+
+  const handleNewGame = React.useCallback(() => {
+    setIsNewGame(true);
+    setAsserts(0);
+    setErrors(0);
+    setPaused(false);
+  }, []);
+
+  const handleStartGame = React.useCallback(() => {
+    setRestart(prevState => !prevState);
+    setIsNewGame(false);
+    handlePageChange(difficulty);
+  }, [difficulty, handlePageChange, setRestart]);
+
+  React.useEffect(() => {
+    const firstCard = cards.find(card => card.isToggle && !card.isDisabled);
+    const secondCard = cards.find(
+      card => card.isToggle && card.id !== firstCard?.id && !card.isDisabled
+    );
+
+    if (!firstCard || !secondCard) return;
+
+    const bothCardsAreValid = !firstCard.isDisabled && !secondCard.isDisabled;
+    if (!bothCardsAreValid) return;
+
+    if (firstCard.title === secondCard.title) {
+      handleSuccess(firstCard, secondCard);
+      return;
+    }
+
+    if (firstCard.title !== secondCard.title) {
+      handleError(firstCard, secondCard);
+    }
+  }, [cards, handleError, handleSuccess]);
 
   const value = React.useMemo(
     () => ({
-      name,
-      setName,
       asserts,
-      setAsserts,
+      difficulty,
       errors,
-      setErrors,
+      handleNewGame,
+      isNewGame,
       isWinner,
-      setIsWinner,
+      name,
+      paused,
+      setAsserts,
+      setDifficulty,
+      setErrors,
+      setIsNewGame,
+      setName,
+      setPaused,
+      handleStartGame,
     }),
-    [asserts, errors, isWinner, name]
+    [
+      asserts,
+      difficulty,
+      errors,
+      handleNewGame,
+      handleStartGame,
+      isNewGame,
+      isWinner,
+      name,
+      paused,
+      setDifficulty,
+      setName,
+    ]
   );
 
   return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
